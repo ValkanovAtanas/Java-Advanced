6b502c2bceb98cabedc31824930685dd8c1d6f0b
Valkanov <valkanovatanas@gmail.com>
2022-01-15 11:19:58 +0200

    new exercise

---
+++ src/BalancedParentheses.java
@@ -0,0 +1,40 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class BalancedParentheses {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String sequence = scanner.nextLine();
+        char[] charArray = sequence.toCharArray();
+        Deque<Character> stack = new ArrayDeque<>();
+        int charArrayLength = charArray.length;
+
+        for (int i = 0; i < charArrayLength; i++) {
+            char currentChar = charArray[i];
+            char lastElement = '?';
+            if (!stack.isEmpty()) {
+                lastElement = stack.peek();
+            }
+            stack.push(currentChar);
+            if (lastElement == '{' && currentChar == '}') {
+                stack.pop();
+                stack.pop();
+            } else if (lastElement == '[' && currentChar == ']') {
+                stack.pop();
+                stack.pop();
+            } else if (lastElement == '(' && currentChar == ')') {
+                stack.pop();
+                stack.pop();
+            }
+        }
+        if (stack.isEmpty()) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+
+        System.out.println();
+    }
+}
