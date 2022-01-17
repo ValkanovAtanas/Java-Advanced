94234058ab8db40d2cd942761fa8a13ae12057cb
Valkanov <valkanovatanas@gmail.com>
2022-01-17 23:41:45 +0200

    new exercise

--- src/SimpleTextEditor.java
+++ src/SimpleTextEditor.java
@@ -6,7 +6,7 @@ public class SimpleTextEditor {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int numOfLines = Integer.parseInt(scanner.nextLine());
-        String myText = "";
+        String myText;
         Deque<String> stack = new ArrayDeque<>();
 
         for (int i = 0; i < numOfLines; i++) {
