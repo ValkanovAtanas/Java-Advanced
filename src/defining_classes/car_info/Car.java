de62ae812b1960af26b79a0059d1c70d459a219a
Atanas <valkanovatanas@gmail.com>
2022-01-28 11:59:25 +0200

    test

--- src/defining_classes/car_info/Car.java
+++ src/defining_classes/car_info/Car.java
@@ -1,5 +1,41 @@
 package defining_classes.car_info;
 
 public class Car {
+    private String brand;
+    private String model;
+    private int horsePower;
 
+    public String toString() {
+        return String.format("The car is: %s %s - %d HP.", this.brand, this.model, this.horsePower);
+    }
+
+    public Car (String brand, String model, int horsePower) {
+        this.brand = brand;
+        this.model = model;
+        this.horsePower = horsePower;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModel () {
+        return this.model;
+    }
+
+    public void setHorsePower (int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public int getHorsePower () {
+        return this.horsePower;
+    }
 }
--- src/defining_classes/car_info/Main.java
+++ src/defining_classes/car_info/Main.java
@@ -5,5 +5,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        int numOfLines = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < numOfLines; i++) {
+            String[] tokens = scanner.nextLine().split("\\s+");
+            String brand = tokens[0];
+            String model = tokens[1];
+            int horsePower = Integer.parseInt(tokens[2]);
+            Car car = new Car(brand, model, horsePower);
+            System.out.println(car);
+        }
     }
 }
