1d17d9cab6b5db2b2d677f2123caa0a178de72b6
Valkanov <valkanovatanas@gmail.com>
2022-03-09 23:21:50 +0200

    change made

--- src/exams/februaryTwentySecond/guild/Guild.java
+++ src/exams/februaryTwentySecond/guild/Guild.java
@@ -3,6 +3,7 @@ package exams.februaryTwentySecond.guild;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Guild {
     private List<Player> roster;
@@ -48,7 +49,28 @@ public class Guild {
         }
     }
 
-//    public Player[] kickPlayerByClass (String clazz) {
-//        Player[] kickedPlayersArray;
-//    }
+    public Player[] kickPlayersByClass(String clazz) {
+        List<Player> kickedPlayersList = new ArrayList<>();
+        for (int i = 0; i < this.roster.size(); i++) {
+            if(this.roster.get(i).getClazz().equals(clazz)) {
+                kickedPlayersList.add(this.roster.get(i));
+                this.roster.remove(i);
+                i--;
+            }
+        }
+        return kickedPlayersList.toArray(new Player[kickedPlayersList.size()]);
+    }
+
+    public int count() {
+        return this.roster.size();
+    }
+
+    public String report() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Players in the guild: ").append(this.name).append(":").append(System.lineSeparator());
+        for (Player player : this.roster) {
+            sb.append(player).append(System.lineSeparator());
+        }
+        return sb.toString().trim();
+    }
 }
