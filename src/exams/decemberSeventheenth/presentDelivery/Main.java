f3e7ee306fa5526fe37a55f1f2c4c869130f1408
Atanas <valkanovatanas@gmail.com>
2022-03-10 21:39:23 +0200

    new exercise

---
+++ src/exams/decemberSeventheenth/presentDelivery/Main.java
@@ -0,0 +1,10 @@
+package exams.decemberSeventheenth.presentDelivery;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
